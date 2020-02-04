6d36e88ef86de37706ffc1d1b381bd6eb113454f
Adrien Rosi <adrienrosi@gmail.com>
2020-02-04 18:07:03 +0100

    Remove entries

--- Entries.php
+++ Entries.php
@@ -47,14 +47,4 @@ abstract class Entries
         'type_occupation_logement_2' => 'Résidence secondaire'
     ];
 
-    /**
-     * @property array
-     */
-    const TYPES_PROJET_EPTZ = [
-        'type_projet_eptz_1' => 'Action seule',
-        'type_projet_eptz_2' => 'Bouquet de 2 travaux',
-        'type_projet_eptz_3' => 'Bouquet de 3 travaux ou plus',
-        'type_projet_eptz_4' => 'Performance énergétique globale',
-        'type_projet_eptz_5' => 'Assainissement non collectif'
-    ];
 }
